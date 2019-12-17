fe15b8ea687d94dbb8907b9697d0132bf2439b0a
John B <john@nexys.ch>
2019-12-17 11:30:02 +0100

    wirh build

---
+++ docs/precache-manifest.533da3407659b8b5eec8ff1b2afd081b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6d1503c711570fdc9b0a7a1ce50a388",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfe83d78ba95939c6c98",
+    "url": "/static/js/2.b0e22b67.chunk.js"
+  },
+  {
+    "revision": "045092ce3fa07afe1276",
+    "url": "/static/js/main.ea4542e4.chunk.js"
+  },
+  {
+    "revision": "166f0a886ded59f6d14c",
+    "url": "/static/js/runtime-main.b64d7e09.js"
+  }
+]);
