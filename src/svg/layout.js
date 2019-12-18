fa9d76dd6b0240ed540b7a15d8d5ba53a35442cb
John B <john@nexys.ch>
2019-12-18 13:36:05 +0100

    new GH Pages version

--- src/svg/layout.js
+++ src/svg/layout.js
@@ -3,13 +3,13 @@ import React from 'react';
 export default props => {
   const { children } = props;
 
-  const onMouseMove = e => {
+  //const onMouseMove = e => {
     //const x = e.screenX - 7;
     //const y = e.screenY - 86;
 
     //const c = { x, y };
     //console.log(c)
-  }
+  //}
 
   return <div style={{
     display: 'inline-block',
@@ -22,7 +22,8 @@ export default props => {
     <svg
       version="1.1"
       viewBox="0 0 800 800" 
-      fdspreserveAspectRatio="xMinYMin meet"
+      {/*preserveAspectRatio="xMinYMin meet"
+      onMouseMove={onMouseMove}*/}
       style={{
         border: '1px solid black',
         display: 'inline-block',
@@ -30,13 +31,6 @@ export default props => {
         top: 0,
         left: 0
       }}
-    >
-
-      {/*<svg
-    style={{border: '1px solid black'}}
-    onMouseMove={onMouseMove}
-    width="800px"
-    height="800px"
-    viewBox="0 0 800 800"
-  >*/}{children}</svg></div>;
+    >{children}</svg>
+  </div>;
 }
