ad8c1f3007777963c793f9575ef3e971fc92bb52
John B <john@nexys.ch>
2019-12-18 01:30:46 +0100

    with ui

---
+++ docs/precache-manifest.8c983720cc3a4b7e0c3847e10ff3e738.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e591ce5038a3942caf6807dff848657b",
+    "url": "/svg/index.html"
+  },
+  {
+    "revision": "ad0712cd1f2ceba83f6d",
+    "url": "/svg/static/js/2.54b58945.chunk.js"
+  },
+  {
+    "revision": "7c042e357f79607b5802",
+    "url": "/svg/static/js/main.13a9f286.chunk.js"
+  },
+  {
+    "revision": "975bbf4869c16b6cee39",
+    "url": "/svg/static/js/runtime-main.25c60ff4.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/svg/precache-manifest.879ffced3dae2a530d07e7af106cf5d4.js"
+  "/svg/precache-manifest.8c983720cc3a4b7e0c3847e10ff3e738.js"
 );
 
 self.addEventListener('message', (event) => {
